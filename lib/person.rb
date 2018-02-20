b2cdcd45afb499ea2f91be93c24f71a8482dbf47
stephenchou-ww <stephen.chou@wework.com>
2018-02-20 05:29:59 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+  def initialize(person_name, person_job)
+  @person_name = person_name
+  @person_job = person_job
+  end
+  
+  def name=(dog_name)
+  @dog_name = dog_name
+  end
+  
+  def name
+    @dog_name
+  end
+  
+  def breed=(dog_breed)
+  @dog_breed = dog_breed
+  end
+  
+  def breed
+    @dog_breed
+  end
+  
+end
