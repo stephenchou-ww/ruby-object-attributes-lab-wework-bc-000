13e68e91c0c648ae46e73edc9b6d068e5b0a6713
stephenchou-ww <stephen.chou@wework.com>
2018-02-20 05:33:42 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,19 +1,19 @@
 class Person
   
   def name=(person_name)
-  @person_name = person_name
+    @name = person_name
   end
   
   def name
-    @person_name
+    @name
   end
   
   def job=(person_job)
-  @person_job = person_job
+    @job = person_job
   end
   
   def job
-    @person_job
+    @job
   end
   
 end
